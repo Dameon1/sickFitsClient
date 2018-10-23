655b656f352a1ebb7ea7d164c61bc1f92eda5308
Dameon1 <dameonmedoza@gmail.com>
2018-10-23 16:16:43 -0700

    injectGlobal styles and CSS in JS

--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import Header from '../components/Header';
-import Meta from '../components/Meta';
+import React, { Component } from 'react';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import Header from './Header';
+import Meta from './Meta';
 
 const theme = {
   red: '#FF0000',
@@ -11,8 +11,7 @@ const theme = {
   offWhite: '#EDEDED',
   maxWidth: '1000px',
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
-}
-
+};
 
 const StyledPage = styled.div`
   background: white;
@@ -25,16 +24,45 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
-export default class Page extends Component {
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
+class Page extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
         <StyledPage>
           <Meta />
-          <Header />        
+          <Header />
           <Inner>{this.props.children}</Inner>
         </StyledPage>
       </ThemeProvider>
-    )
+    );
   }
 }
+
+export default Page;
