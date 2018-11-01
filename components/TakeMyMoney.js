a4d50a457aac41703c270c148d153d4ba4d64608
Dameon1 <dameonmedoza@gmail.com>
2018-11-01 14:41:35 -0700

    charging CC's on the server side

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -9,32 +9,62 @@ import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
+
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!){
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
-}
+};
 
 class TakeMyMoney extends React.Component {
 
-  onToken = (res) => {
-    console.log(res.id)
-  }
+  onToken = (res, createOrder) => {
+    createOrder({
+      variables: {
+        token: res.id
+      },
+    }).catch(err => {
+      alert(err.message);
+    });
+  };
 
   render(){
     return (
       <User>
         {({ data: { me }}) => (
+          <Mutation 
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries = {[{ query : CURRENT_USER_QUERY }]}
+            >
+            {(createOrder, {loading, error}) => (
+
+         
           <StripeCheckout
             amount={calcTotalPrice(me.cart)}
             name='Sick Fits'
             description={`Order of ${totalItems(me.cart)} items!`}
-            //TODO => image={me.cart[0].item ? me.cart[0].item.image : '' }
+            image={me.cart.length > 0 ? me.cart[0].item.image : '' }
             stripeKey="pk_test_3Cj2jecyKAMPvSsZtMh9kcQQ"
             currency='USD'
             email={me.email}
-            token={res => this.onToken(res)}
+            token={res => this.onToken(res, createOrder)}
           >
             {this.props.children}
           </StripeCheckout>
+            )}
+          </Mutation>
         )}
       </User>
     )
