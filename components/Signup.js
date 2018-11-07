20c369dfae5704479dfc6362c69c2d3ae7da2820
Dameon1 <dameonmedoza@gmail.com>
2018-11-06 21:11:23 -0700

    signup component tests

--- components/Signup.js
+++ components/Signup.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
@@ -6,7 +6,7 @@ import Error from './ErrorMessage';
 import { CURRENT_USER_QUERY } from './User';
 
 const SIGNUP_MUTATION = gql`
-  mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!){
+  mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
     signup(email: $email, name: $name, password: $password) {
       id
       email
@@ -18,68 +18,69 @@ const SIGNUP_MUTATION = gql`
 class Signup extends Component {
   state = {
     name: '',
-    password: '',
     email: '',
+    password: '',
   };
   saveToState = e => {
-    this.setState({
-      [e.target.name]:e.target.value,
-    })   
+    this.setState({ [e.target.name]: e.target.value });
   };
-
   render() {
     return (
-      <Mutation 
-        mutation={SIGNUP_MUTATION} 
-        variables= {this.state} 
-        refetchQueries={[{ query: CURRENT_USER_QUERY }]} 
+      <Mutation
+        mutation={SIGNUP_MUTATION}
+        variables={this.state}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
       >
-        {(signup, { error, loading }) => (     
-          <Form method="post" onSubmit={async e => {
-            e.preventDefault();
-            await signup();
-            this.setState({name: "", email: "", password: "",})
-          }}>         
+         {(signup, { error, loading }) => (
+          <Form
+            method="post"
+            onSubmit={async e => {
+              e.preventDefault();
+              await signup();
+              this.setState({ name: '', email: '', password: '' });
+            }}
+          >       
             <fieldset disabled={loading} aria-busy={loading}>
-              <h2>Sign up for Account</h2>
+              <h2>Sign Up for An Account</h2>
               <Error error={error} />
               <label htmlFor="email">
                 Email
-                <input 
+                <input
                   type="email"
                   name="email"
-                  placeholder="e-mail"
+                  placeholder="email"
                   value={this.state.email}
                   onChange={this.saveToState}
-                />            
+                />
               </label>
               <label htmlFor="name">
                 Name
-                <input 
+                <input
                   type="text"
                   name="name"
                   placeholder="name"
                   value={this.state.name}
                   onChange={this.saveToState}
-                />            
+                />
               </label>
               <label htmlFor="password">
                 Password
-                <input 
+                <input
                   type="password"
                   name="password"
                   placeholder="password"
                   value={this.state.password}
                   onChange={this.saveToState}
                 />
-              </label>
+              </label>            
+              <button type="submit">Sign Up!</button>
             </fieldset>
-            <button type="submit">Sign Up!</button>
           </Form>
-        )};
+        )}
       </Mutation>
     );
-  };
-};
+  }
+}
 
 export default Signup;
+export { SIGNUP_MUTATION }
