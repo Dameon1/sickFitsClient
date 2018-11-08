89b8f594a06373c07b9d566628df02cbd917efe8
Dameon1 <dameonmedoza@gmail.com>
2018-11-07 21:54:57 -0700

    added Cart testing

--- components/RemoveFromCart.js
+++ components/RemoveFromCart.js
@@ -7,7 +7,7 @@ import { CURRENT_USER_QUERY } from './User';
 
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation removeFromCart($id: ID!) {
-    removeFromCart(id: $id){
+    removeFromCart(id: $id) {
       id
     }
   }
@@ -18,47 +18,54 @@ const BigButton = styled.button`
   background: none;
   border: 0;
   &:hover {
-    color : ${props => props.theme.red};
-    cursor: pointer
+    color: ${props => props.theme.red};
+    cursor: pointer;
   }
 `;
 
 class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
-  }
+  };
+  // This gets called as soon as we get a response back from the server after a mutation has been performed
   update = (cache, payload) => {
-    const data = cache.readQuery({
-      query: CURRENT_USER_QUERY
-    });
+    // 1. first read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    // 2. remove that item from the cart
     const cartItemId = payload.data.removeFromCart.id;
     data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    // 3. write it back to the cache
     cache.writeQuery({ query: CURRENT_USER_QUERY, data });
   };
-
-  render(){
+  render() {
     return (
-      <Mutation mutation={REMOVE_FROM_CART_MUTATION} 
-                variables={{id: this.props.id}}
-                update={this.update} 
-                optimisticResponse={{
-                  __typename: 'Mutation',
-                  removeFromCart: {
-                    __typename: 'CartItem',
-                    id: this.props.id,
-                  }
-                }}
+      <Mutation
+        mutation={REMOVE_FROM_CART_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id,
+          },
+        }}
       >
-        {(removeFromCart,{loading, error}) => (
-          <BigButton title="Delete Item" disabled={loading} onClick={() => {                    
-                     removeFromCart().catch(err => alert(err.message));
-                     }}>
+        {(removeFromCart, { loading, error }) => (
+          <BigButton
+            disabled={loading}
+            onClick={() => {
+              removeFromCart().catch(err => alert(err.message));
+            }}
+            title="Delete Item"
+          >
             &times;
           </BigButton>
-        )};
+        )}
       </Mutation>
     );
-  };
-};
+  }
+}
 
 export default RemoveFromCart;
+export { REMOVE_FROM_CART_MUTATION };
