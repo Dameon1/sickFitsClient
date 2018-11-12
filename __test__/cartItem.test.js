eda3ab37640faf55cf8624e50a43413c619115ef
Dameon1 <dameonmedoza@gmail.com>
2018-11-11 22:10:45 -0700

    cartItem testing

---
+++ __test__/cartItem.test.js
@@ -0,0 +1,23 @@
+
+import wait from 'waait';
+import toJSON from 'enzyme-to-json';
+import { MockedProvider } from 'react-apollo/test-utils';
+import CartItem from '../components/CartItem';
+import { fakeCartItem } from '../lib/testUtils';
+import { shallow, mount } from 'enzyme';
+
+const fakeItem = fakeCartItem();
+
+describe('<CartItem/>', () => {
+  it('renders and matches snapshot', async () => {
+    const wrapper = mount(
+        <MockedProvider >
+          <CartItem cartItem={fakeItem}/>
+        </MockedProvider>
+      );
+       await wait();
+       wrapper.update();
+       expect(toJSON(wrapper.find('CartItem'))).toMatchSnapshot();
+       expect(wrapper.find('CartItem')).toHaveLength(1);
+      });
+  });
