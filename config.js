59b3483faf1059770d91b42f679f6e4025e6a50d
Dameon1 <dameonmedoza@gmail.com>
2018-11-14 11:27:28 -0700

    changed export

--- config.js
+++ config.js
@@ -2,10 +2,6 @@
 export const endpoint = `http://localhost:4444`;
 export const prodEndpoint = 'https://sickfits-production.herokuapp.com/';
 export const perPage = 4;
-
-module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/somewhere` : '',
-}
 const debug = process.env.NODE_ENV !== 'production';
 const assetPrefix = debug ? '' : '/sickFitsClient/';
 
