319d23d38884bda5dec04a27a063e9afbdfef495
Dameon Mendoza <dameonmendoza@gmail.com>
2020-10-24 08:59:23 -0700

    changed title to rick sits

--- components/Header.js
+++ components/Header.js
@@ -59,7 +59,7 @@ const Header = () => (
     <div className='bar'>
       <Logo>
         <Link href="/">
-          <a>Sick Fits</a>
+          <a>Rick Sits</a>
         </Link>
       </Logo>      
       <Nav />
